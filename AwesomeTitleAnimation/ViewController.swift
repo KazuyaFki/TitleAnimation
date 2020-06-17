9cb564c404bc48acf1331e713dce251f21278d3f
KazuyaFki <complete501xx@yahoo.co.jp>
2020-06-17 12:28:40 +0900

    Initial Commit

---
+++ AwesomeTitleAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AwesomeTitleAnimation
+//
+//  Created by Kazuya Fukui on 2020/06/17.
+//  Copyright © 2020 Kazuya Fukui. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
